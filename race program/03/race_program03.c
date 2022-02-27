c7ef75fdbe09ae519a6169d708cde6be0b3c1855
MarceloJSCesar <jsdevmarcelo@gmail.com>
2022-02-27 13:40:36 -0100

    03 done

--- race program/03/race_program03.c
+++ race program/03/race_program03.c
@@ -6,22 +6,22 @@ int main(void){
     scanf("%d", &place);
     /*
         Implementing with grafic logical 
-        y = 12 - 2 * x(place) for place 1 and 2
-        y = 9 - x(place) for place 3,4,5,6,7,8 
+        y = 12 - 2 * x(place) for place 1, 2 and 3
+        y = 9 - x(place) for place 4,5,6,7,8 
         
     */
     switch(place){
-        case 1: case 2:
+        case 1: case 2: case 3:
         points = 12 - 2 * place;
-        printf("Ficaste com : %d points", points);
+        printf("You got: %d points", points);
         break;
-        case 3: case 4: case 5: case 6: case 7: case 8:
+        case 4: case 5: case 6: case 7: case 8:
         points = 9 - place; 
-        printf("Ficaste com : %d points", points);
+        printf("You got: %d points", points);
         break;
         default:
         points = 0;
-        printf("Você não ganhou nenhum ponto");
+        printf("You got %d point", points);
     }
 
     return 0;
